db85a3d6b4ba5ebf278756ecb2e9845ecad1b914
Michele Akeson <akesonm@mcmaster.ca>
2022-09-03 23:50:16 -0400

    x

--- src/Actions/BackendActions.js
+++ src/Actions/BackendActions.js
@@ -2,7 +2,7 @@
 
 import {nanoid} from 'nanoid'
 
-const url = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000/"
+const url = process.env.REACT_APP_BACKEND_URL || "https://my-trello-app-clone.herokuapp.com/" || "http://localhost:5000/"
 console.log(url)
 const profilePath = "profile"
 const boardPath = "board"
